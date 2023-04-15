7d005f391641e13d25304a6ae63f38a186899019
Nume-stack <julianguanume@yahoo.es>
2023-04-15 15:41:32 -0500

    se agrega el proyecto

---
+++ data/data.js
@@ -0,0 +1,20 @@
+const data =[
+    {
+        question : 'Tristeza',
+        
+        choices : {
+            'No me siento triste' : 0,
+            'Me siento triste gran parte del tiempo' : 1,
+            'Me siento triste todo el tiempo' : 2,
+            'Me siento tan triste o soy tan infeliz que no puedo soportarlo' : 3
+        },
+
+        answers : {
+            'No me siento triste' : 0,
+            'Me siento triste gran parte del tiempo' : 1,
+            'Me siento triste todo el tiempo' : 2,
+            'Me siento tan triste o soy tan infeliz que no puedo soportarlo' : 3
+        }
+        
+    }
+]
